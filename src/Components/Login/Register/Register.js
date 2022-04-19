44066ab6f9d71713f61aa62e933c52218bf6692a
KawsarAlHasan <kawsaralhasan.420@gmail.com>
2022-04-19 12:07:30 +0600

    added and design register pages

--- src/Components/Login/Register/Register.js
+++ src/Components/Login/Register/Register.js
@@ -1,11 +1,54 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import { Button, Form } from 'react-bootstrap';
+import { Link, useNavigate } from 'react-router-dom';
 
-const componentName = (props) => {
+
+const Register = (props) => {
+
+    const emailRef = useRef('');
+    const passwordRef = useRef('');
+    const navigate = useNavigate();
+
+    const handleSubmit = evan => {
+        evan.preventDefault();
+        const email = emailRef.current.value;
+        const password = passwordRef.current.value;
+
+        console.log(email, password);
+    }
+
+    const navigateLogin = evant => {
+        navigate('/login');
+    }
+
     return (
-        <div>
-            <h2>This is Register</h2>
+        <div className='container w-50 mx-auto mt-3'>
+            <h2 className='text-center'>Please <span className='text-danger'>Register</span></h2>
+            <Form onSubmit={handleSubmit}>
+                <Form.Group className="mb-3" controlId="formBasicName">
+                    <Form.Label>Your Name</Form.Label>
+                    <Form.Control type="text" placeholder="Enter Name"/>
+                </Form.Group>
+
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form.Label>Email address</Form.Label>
+                    <Form.Control ref={emailRef} type="email" placeholder="Enter email" required/>
+                </Form.Group>
+
+                <Form.Group className="mb-3" controlId="formBasicPassword">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control ref={passwordRef} type="password" placeholder="Password" required/>
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                    <Form.Check type="checkbox" label="Check me out" />
+                </Form.Group>
+                <Button variant="primary" type="submit">
+                    Submit
+                </Button>
+            </Form>
+            <p>Already have an  account? <Link to="/login" className='text-danger text-decoration-none' onClick={navigateLogin}>Please Login</Link></p>
         </div>
     );
 };
 
-export default componentName;
+export default Register;
